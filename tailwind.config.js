5fb6bf54bc0a745f390945af25068f7f855ae262
chris0292 <christian.chramirez@gmail.com>
2023-07-03 12:37:05 -0500

    Taildwind instalado

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors:{
+      "blue-pry": "#0045A4",
+      "dark-blue-pry": "#051831",
+    },
+  },
+  plugins: [],
+}
+
